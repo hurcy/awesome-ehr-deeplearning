996a814d71d9251b9dfe59a21a9f0d669ab8bba6
hurcy <cinyoung.hur@gmail.com>
2019-04-26 22:04:11 +0900

    travis config

--- .travis/push.py
+++ .travis/push.py
@@ -5,17 +5,17 @@ setup_git() {
   git config --global user.name ${NAME}
 }
 
-commit_website_files() {
+commit_files() {
   cp README.md docs/
   git add . *.md
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/hurcy/awesome-ehr-deeplearning > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/hurcy/awesome-ehr-deeplearning.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master 
 }
 
 setup_git
-commit_website_files
+commit_files
 upload_files
